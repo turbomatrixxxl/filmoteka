83c97b687e5b3e9673589295f29fa090d63f13dd
AdrianaIngrid <150287447+AdrianaIngrid@users.noreply.github.com>
2024-06-10 21:01:28 +0300

    1

--- src/index.js
+++ src/index.js
@@ -1,45 +1,93 @@
-const filmsGallery = document.querySelector('.hero-films-gallery');
-const fetch = require('node-fetch');
+
+import Notiflix from 'notiflix';
+import { heroLoad } from './apiKey';
+//const fetch = require('node-fetch');
 const url = 'https://api.themoviedb.org/3/configuration';
-const options = {
-  method: 'GET',
-  headers: {
-    accept: 'application/json',
-    Authorization:
-      'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3NWIyM2Q3ZGY4MTY5YTRmYTJlNmY5YjAzYjZmZGY1OCIsInN1YiI6IjY2NjQ0ODdiM2ZiNTViYmExODA5ZWQ5YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.KeSc5hLobWtGaw46ZiucUM4d-MwlPh_Hd_I9wfwm5gw',
-  },
+const heroRefs = {
+  heroFilmsList: document.querySelector('.hero-films-list'),
+  heroPaginationList: document.querySelector('.hero-pagination-list'),
+  heroMessageList: document.querySelector('.hero-films-message'),
+
 };
 
 
   
-function getFilms(hits) {
-    const markUp = hits
-        .map(({item}) => {
-      return `<div class="hero-photo-card">
-            <a href ="${item.base_url}">
-  <img src="${item.secure_base_url}" alt="${item.change_keys[3]}" width = "${item.backdrop_sizes}
-" loading="lazy" />
-                </a>
-  <div class="hero-film-info">
-    <p class="hero-info-item">
-      <b>Title</b> </br> ${item.change_keys[0]}</p>
-    <p class="hero-info-item">
-      <b>Genre</b> </br> ${item.change_keys[19]}</p>
-    <p class="hero-info-item">
-      <b>Released</b></br> ${item.change_keys[37]}</p>
-      </div>
-</div>`;
-    })
-    .join('');
-  filmsGallery.insertAdjacentElement('beforeend', markUp);
-}
-fetch(url, options)
-  .then(res => res.json())
-  .then(json => console.log(json))
-  .catch(err => console.error('error:' + err));
-getFilms();
- //async function logMovies() {
-//  const response = await fetch(url,options);
-//  const movies = await response.json();
- // console.log("movies", movies);
-//}
+//function getFilms(hits) {
+//     const markUp = hits
+//         .map(item => {
+//       return `<div class="hero-photo-card">
+//             <a href ="${item.options.base_url}">
+//   <img src="${item.options.secure_base_url}" alt="${item.options.change_keys[3]}" width = "${item.options.backdrop_sizes}
+// " loading="lazy" />
+//                 </a>
+//   <div class="hero-film-info">
+//     <p class="hero-info-item">
+//       <b>Title</b> </br> ${item.options.change_keys[0]}</p>
+//     <p class="hero-info-item">
+//       <b>Genre</b> </br> ${item.options.change_keys[19]}</p>
+//     <p class="hero-info-item">
+//       <b>Released</b></br> ${item.options.change_keys[37]}</p>
+//       </div>
+// </div>`;
+//     })
+//     .join('');
+//   filmsGallery.insertAdjacentElement('beforeend', markUp);
+// }
+
+// getFilms();
+
+const heroPathForImg = 'https://image.tmdb.org/t/p/w342/';
+const heroImgDefault = './images/default-opt.jpg';
+export default {
+  additemList(listFilms, detalsFilm) {
+    if (!listFilms || !listFilms.length) {
+      Notiflix.Notify.warning(`Error Warning There is no film list!`);
+
+      heroRefs.heroFilmsList.innerHTML = '';
+      heroRefs.heroPaginationList.innerHTML = '';
+      return;
+    }
+    const resultForMarkup = listFilms.map(elem => {
+      const genresFilm = detalsFilm.find(item => item.id === elem.id).genres;
+
+      const genres = genresFilm.length > 2 ? [...genresFilm.slice(0, 2), { id: 999, name: 'Other' }] : genresFilm.length
+        ? genresFilm : [...genresFilm.slice(0, 2), { id: 998, name: 'N/A' }];
+      return {
+        ...elem,
+        poster_path: elem.poster_path
+          ? heroPathForImg + elem.poster_path
+          : false,
+        release_date: elem.release_date ? elem.release_date.slice(0, 4) : 'N/A',
+        genres,
+      };
+    });
+ 
+    heroRefs.heroFilmsList.innerHTML = '';
+    heroRefs.heroFilmsList.insertAdjacentHTML('beforeend', filmCard(resultForMarkup));
+
+  },
+  addLibraryList(listFilms) {
+    if (!listFilms || !listFilms.length) {
+      heroRefs.heroFilmsList.innerHTML = '';
+      heroRefs.heroPaginationList.innerHTML = '';
+      const heroNameLibrary = heroLoad('currentRequest');
+      heroRefs.heroMessageList.classList.remove('is-hidden');
+      Notiflix.Notify(`Oops! Your "${heroNameLibrary}" library is empty!`);
+      return;
+    }
+    heroRefs.heroMessageList.classList.add('is-hidden');
+
+    const heroResulOfMarkup = listFilms.map(elem => {
+      const genres = elem.genres.length > 2 ? [...elem.genres.slice(0, 2), { id: 999, name: 'Other' }] : elem.genres.length
+        ? elem.genres : [...elem.genres.slice(0, 2), { id: 998, name: 'N/A' }];
+      
+      return {
+        ...elem, poster_path: elem.poster_path ? heroPathForImg + elem.poster_path : heroImgDefault,
+        release_date: elem.release_date ? elem.release_date.slice(0, 4) : 'N/A',
+        genres,
+      };
+    });
+    heroRefs.heroFilmsList.innerHTML = '';
+    heroRefs.heroFilmsList.insertAdjacentHTML('beforeend',filmCard(resultForMarkup));
+  },
+};
