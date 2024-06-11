3d527f6619bcbe1ab0baeb3ed35845845b223a09
Silvianulul <silvi_mitrica@yahoo.com>
2024-06-11 21:43:55 +0300

    header

---
+++ src/index.js
@@ -0,0 +1,21 @@
+//Scroll button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById('scrollToTopBtn').style.display = 'block';
+  } else {
+    document.getElementById('scrollToTopBtn').style.display = 'none';
+  }
+}
+
+function scrollToTop() {
+  const c = document.documentElement.scrollTop || document.body.scrollTop;
+  if (c > 0) {
+    window.requestAnimationFrame(scrollToTop);
+    window.scrollTo(0, c - c / 8);
+  }
+}
+///////////////
