1864f1dfe27d5523470e955a08c62f080c9195a0
Silvianulul <silvi_mitrica@yahoo.com>
2024-06-12 19:54:43 +0300

    header

--- src/index.js
+++ src/index.js
@@ -23,29 +23,29 @@ scrollToTopButton.addEventListener('click', function () {
 ///////////////
 
 //Library button
-const libraryButton = document.querySelector('.library-button');
-const watchedButton = document.querySelector('.watched-button');
-const queueButton = document.querySelector('.queue-button');
-const iconTitle = document.querySelector('.icon-title-div');
+const headerLibraryButton = document.querySelector('.library-button');
+const headerWatchedButton = document.querySelector('.watched-button');
+const headerQueueButton = document.querySelector('.queue-button');
+const headerIconTitle = document.querySelector('.icon-title-div');
 function isMobile() {
   return window.innerWidth <= 768;
 }
 
-libraryButton.addEventListener('click', function (e) {
-  queueButton.classList.remove('is-hidden');
-  watchedButton.classList.remove('is-hidden');
+headerLibraryButton.addEventListener('click', function (e) {
+  headerQueueButton.classList.remove('is-hidden');
+  headerWatchedButton.classList.remove('is-hidden');
 });
 function updateMargin() {
   if (!isMobile()) {
     //Reseteaza daca pagina are width mai mare de 768px
-    iconTitle.style.marginBottom = '16px';
+    headerIconTitle.style.marginBottom = '16px';
   }
 }
-libraryButton.addEventListener('click', function () {
+headerLibraryButton.addEventListener('click', function () {
   // Check if the device is mobile
   if (isMobile()) {
     // Change the margin-bottom of the target element
-    iconTitle.style.marginBottom = '74px'; // New margin-bottom  for mobile
+    headerIconTitle.style.marginBottom = '74px'; // New margin-bottom  for mobile
   }
 });
 
