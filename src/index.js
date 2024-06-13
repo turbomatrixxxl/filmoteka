d469637dbbef632803c374ac8111e6c04548235c
Dragos0129 <dragosbaba@gmail.com>
2024-06-14 00:20:37 +0300

    commit 5

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 //PAGE CHANGER
 // selecting required element
 const element = document.querySelector('.pagination ul');
-let totalPages = 5;
-let page = 1;
+let totalPages = 20;
+let page = 10;
 
 //calling function with passing parameters and adding inside element which is ul tag
 element.innerHTML = createPagination(totalPages, page);
