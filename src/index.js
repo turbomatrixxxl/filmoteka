d0f456b543f7ea3a122cad2fd68f4b910229cce4
AdrianaIngrid <150287447+AdrianaIngrid@users.noreply.github.com>
2024-06-15 18:55:31 +0300

    heroo

--- src/index.js
+++ src/index.js
@@ -1,93 +1,103 @@
+// import Notiflix from 'notiflix';
 
-import Notiflix from 'notiflix';
-import { heroLoad } from './apiKey';
-//const fetch = require('node-fetch');
-const url = 'https://api.themoviedb.org/3/configuration';
+const fetch = require('node-fetch');
+// const url = 'https://api.themoviedb.org/3/configuration';
+
 const heroRefs = {
   heroFilmsList: document.querySelector('.hero-films-list'),
   heroPaginationList: document.querySelector('.hero-pagination-list'),
   heroMessageList: document.querySelector('.hero-films-message'),
-
 };
 
+const urlImg = 'http://image.tmdb.org/t/p/w500';
+const urlGenres = 'https://api.themoviedb.org/3/genre/movie/list?language=en';
 
-  
-//function getFilms(hits) {
-//     const markUp = hits
-//         .map(item => {
-//       return `<div class="hero-photo-card">
-//             <a href ="${item.options.base_url}">
-//   <img src="${item.options.secure_base_url}" alt="${item.options.change_keys[3]}" width = "${item.options.backdrop_sizes}
-// " loading="lazy" />
-//                 </a>
-//   <div class="hero-film-info">
-//     <p class="hero-info-item">
-//       <b>Title</b> </br> ${item.options.change_keys[0]}</p>
-//     <p class="hero-info-item">
-//       <b>Genre</b> </br> ${item.options.change_keys[19]}</p>
-//     <p class="hero-info-item">
-//       <b>Released</b></br> ${item.options.change_keys[37]}</p>
-//       </div>
-// </div>`;
-//     })
-//     .join('');
-//   filmsGallery.insertAdjacentElement('beforeend', markUp);
+const url =
+  'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc';
+const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization:
+      'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3NWIyM2Q3ZGY4MTY5YTRmYTJlNmY5YjAzYjZmZGY1OCIsInN1YiI6IjY2NjQ0ODdiM2ZiNTViYmExODA5ZWQ5YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.KeSc5hLobWtGaw46ZiucUM4d-MwlPh_Hd_I9wfwm5gw',
+  },
+};
+
+// {
+//     "adult": false,
+//     "backdrop_path": "/fqv8v6AycXKsivp1T5yKtLbGXce.jpg",
+//     "genre_ids": [
+//         878,
+//         12,
+//         28
+//     ],
+//     "id": 653346,
+//     "original_language": "en",
+//     "original_title": "Kingdom of the Planet of the Apes",
+//     "overview": "Several generations in the future following Caesar's reign, apes are now the dominant species and live harmoniously while humans have been reduced to living in the shadows. As a new tyrannical ape leader builds his empire, one young ape undertakes a harrowing journey that will cause him to question all that he has known about the past and to make choices that will define a future for apes and humans alike.",
+//     "popularity": 3327.202,
+//     "poster_path": "/gKkl37BQuKTanygYQG1pyYgLVgf.jpg",
+//     "release_date": "2024-05-08",
+//     "title": "Kingdom of the Planet of the Apes",
+//     "video": false,
+//     "vote_average": 6.815,
+//     "vote_count": 1032
 // }
 
-// getFilms();
+function getFilms(hits, genresLabels) {
+  console.log(hits);
 
-const heroPathForImg = 'https://image.tmdb.org/t/p/w342/';
-const heroImgDefault = './images/default-opt.jpg';
-export default {
-  additemList(listFilms, detalsFilm) {
-    if (!listFilms || !listFilms.length) {
-      Notiflix.Notify.warning(`Error Warning There is no film list!`);
+  const markUp = hits
+    .map(item => {
+      return `<div class="hero-photo-card">
+            
+  <img src="${urlImg}${item.poster_path}" alt="${item.title}"  class="hero-img"
+" loading="lazy" />
+                
+  <div class="hero-film-info">
+    <p class="hero-info-item-title">
+       ${item.title}
+      </p>
+    <p class="hero-info-item-vote">
+      <b> Vote/Votes</b> ${item.vote_average}/${item.vote_count}
+      </p>
+    <p class="hero-info-item-popularity">
+      <b>Popularity</b>${item.popularity}
+      </p>
+      <p class="hero-info-item">
 
-      heroRefs.heroFilmsList.innerHTML = '';
-      heroRefs.heroPaginationList.innerHTML = '';
-      return;
-    }
-    const resultForMarkup = listFilms.map(elem => {
-      const genresFilm = detalsFilm.find(item => item.id === elem.id).genres;
+      </p>
+    <p class="hero-info-item-genre">
+      <b>Genre</b> ${item.genre_ids.map(id => genresLabels[id] + ', ')}
+      </p>
+    <p class="hero-info-item-release">
+       ${item.release_date}
+      </p>
+      <p class="hero-info-item-description">
+      <b class = "hero-about">About</b>${item.overview}
+      </p>
+      <div>
+      </div>
+      <button class="hero-button-watched" type= submit>Add to Watched</button>
+      <button class= "hero-button-queue" type = submit>Add to queue</button>
+      </div>
+</div>`;
+    })
+    .join('');
+  heroRefs.heroFilmsList.innerHTML = markUp;
+}
 
-      const genres = genresFilm.length > 2 ? [...genresFilm.slice(0, 2), { id: 999, name: 'Other' }] : genresFilm.length
-        ? genresFilm : [...genresFilm.slice(0, 2), { id: 998, name: 'N/A' }];
-      return {
-        ...elem,
-        poster_path: elem.poster_path
-          ? heroPathForImg + elem.poster_path
-          : false,
-        release_date: elem.release_date ? elem.release_date.slice(0, 4) : 'N/A',
-        genres,
-      };
-    });
- 
-    heroRefs.heroFilmsList.innerHTML = '';
-    heroRefs.heroFilmsList.insertAdjacentHTML('beforeend', filmCard(resultForMarkup));
-
-  },
-  addLibraryList(listFilms) {
-    if (!listFilms || !listFilms.length) {
-      heroRefs.heroFilmsList.innerHTML = '';
-      heroRefs.heroPaginationList.innerHTML = '';
-      const heroNameLibrary = heroLoad('currentRequest');
-      heroRefs.heroMessageList.classList.remove('is-hidden');
-      Notiflix.Notify(`Oops! Your "${heroNameLibrary}" library is empty!`);
-      return;
-    }
-    heroRefs.heroMessageList.classList.add('is-hidden');
-
-    const heroResulOfMarkup = listFilms.map(elem => {
-      const genres = elem.genres.length > 2 ? [...elem.genres.slice(0, 2), { id: 999, name: 'Other' }] : elem.genres.length
-        ? elem.genres : [...elem.genres.slice(0, 2), { id: 998, name: 'N/A' }];
-      
-      return {
-        ...elem, poster_path: elem.poster_path ? heroPathForImg + elem.poster_path : heroImgDefault,
-        release_date: elem.release_date ? elem.release_date.slice(0, 4) : 'N/A',
-        genres,
-      };
-    });
-    heroRefs.heroFilmsList.innerHTML = '';
-    heroRefs.heroFilmsList.insertAdjacentHTML('beforeend',filmCard(resultForMarkup));
-  },
-};
+var promises = [url, urlGenres].map(url =>
+  fetch(url, options).then(res => res.json())
+);
+Promise.all(promises).then(([movies, genresdata]) => {
+  const genresLabels = [];
+  genresdata.genres.forEach(element => {
+    genresLabels[element.id] = element.name;
+  });
+  if (movies.success === false) {
+    heroRefs.heroMessageList.innerHTML = movies.status_message;
+  } else {
+    getFilms(movies.results, genresLabels);
+  }
+});
