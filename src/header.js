060fccb566b3574f310ae3218eda195329df3eed
Radu Bogdan Naramzoiu <turbomatrixxxl@gmail.com>
2024-06-18 11:39:17 +0300

    updated pagination

--- src/header.js
+++ src/header.js
@@ -1,55 +1,78 @@
-const body = document.body;
+//PAGE CHANGER
+// selecting required element
+const element = document.querySelector('.pagination ul');
+let totalPages = 20;
 
-const darkThemeButton = document.querySelector('.checkbox');
-
-const libraryButton = document.querySelector('.header-library-button');
-
-const headerBottomButtonsContainer = document.querySelector(
-  '.header-bottom-buttons-container'
-);
-
-const headerWatchedButton = document.querySelector('.header-watched-button');
-
-const headerClearWatchedButton = document.querySelector(
-  '.header-clear-watched-button'
-);
-
-export const headerQueuedButton = document.querySelector(
-  '.header-queue-button'
-);
-
-const headerClearQueuedButton = document.querySelector(
-  '.header-clear-queue-button'
-);
-
-libraryButton.addEventListener('click', ev => {
-  headerBottomButtonsContainer.classList.toggle('is-hidden');
-});
-
-darkThemeButton.addEventListener('click', ev => {
-  var isChecked = darkThemeButton.checked;
-
-  if (isChecked) {
-    body.style.backgroundColor = 'var(--gray)';
-  } else {
-    body.style.backgroundColor = 'var(--white)';
+//calling function with passing parameters and adding inside element which is ul tag
+element.innerHTML = createPagination(totalPages, page);
+function createPagination(totalPages, page) {
+  let liTag = '';
+  let active;
+  let beforePage = page - 1;
+  let afterPage = page + 1;
+  if (page > 1) {
+    //show the next button if the page value is greater than 1
+    liTag += `<li class="btn prev" onclick="createPagination(totalPages, ${
+      page - 1
+    })"><span><i class="fas fa-angle-left"></i> Prev</span></li>`;
   }
-});
 
-headerWatchedButton.addEventListener('click', ev => {
-  headerClearWatchedButton.classList.toggle('is-hidden');
-  if (headerClearQueuedButton.classList.contains('is-hidden')) {
-    return;
-  } else {
-    headerClearQueuedButton.classList.toggle('is-hidden');
+  if (page > 2) {
+    //if page value is less than 2 then add 1 after the previous button
+    liTag += `<li class="first numb" onclick="createPagination(totalPages, 1)"><span>1</span></li>`;
+    if (page > 3) {
+      //if page value is greater than 3 then add this (...) after the first li or page
+      liTag += `<li class="dots"><span>...</span></li>`;
+    }
   }
-});
 
-headerQueuedButton.addEventListener('click', ev => {
-  headerClearQueuedButton.classList.toggle('is-hidden');
-  if (headerClearWatchedButton.classList.contains('is-hidden')) {
-    return;
-  } else {
-    headerClearWatchedButton.classList.toggle('is-hidden');
+  // how many pages or li show before the current li
+  if (page == totalPages) {
+    beforePage = beforePage - 2;
+  } else if (page == totalPages - 1) {
+    beforePage = beforePage - 1;
   }
-});
+  // how many pages or li show after the current li
+  if (page == 1) {
+    afterPage = afterPage + 2;
+  } else if (page == 2) {
+    afterPage = afterPage + 1;
+  }
+
+  for (var plength = beforePage; plength <= afterPage; plength++) {
+    if (plength > totalPages) {
+      //if plength is greater than totalPage length then continue
+      continue;
+    }
+    if (plength == 0) {
+      //if plength is 0 than add +1 in plength value
+      plength = plength + 1;
+    }
+    if (page == plength) {
+      //if page is equal to plength than assign active string in the active variable
+      active = 'active';
+    } else {
+      //else leave empty to the active variable
+      active = '';
+    }
+    liTag += `<li class="numb ${active}" onclick="createPagination(totalPages, ${plength})"><span>${plength}</span></li>`;
+  }
+
+  if (page < totalPages - 1) {
+    //if page value is less than totalPage value by -1 then show the last li or page
+    if (page < totalPages - 2) {
+      //if page value is less than totalPage value by -2 then add this (...) before the last li or page
+      liTag += `<li class="dots"><span>...</span></li>`;
+    }
+    liTag += `<li class="last numb" onclick="createPagination(totalPages, ${totalPages})"><span>${totalPages}</span></li>`;
+  }
+
+  if (page < totalPages) {
+    //show the next button if the page value is less than totalPage(20)
+    liTag += `<li class="btn next" onclick="createPagination(totalPages, ${
+      page + 1
+    })"><span>Next <i class="fas fa-angle-right"></i></span></li>`;
+  }
+  element.innerHTML = liTag; //add li tag inside ul tag
+  return liTag; //reurn the li tag
+}
--- src/index.js
+++ src/index.js
@@ -1146,50 +1146,51 @@ function renderWatchQueueCards(params) {
 }
 
 // PAGE-CHANGER
-import Pagination from 'tui-pagination'; /* ES6 */
 
-const container = document.getElementById('tui-pagination-container');
+// import Pagination from 'tui-pagination'; /* ES6 */
 
-const pagOptions = {
-  // below default value of options
-  totalItems: 400,
-  itemsPerPage: 20,
-  visiblePages: 20,
-  page: 1,
-  centerAlign: false,
-  firstItemClassName: 'tui-first-child',
-  lastItemClassName: 'tui-last-child',
-  template: {
-    page: '<a href="#" class="tui-page-btn">{{page}}</a>',
-    currentPage:
-      '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
-    moveButton:
-      '<a href="#" class="tui-page-btn tui-{{type}}">' +
-      '<span class="tui-ico-{{type}}">{{type}}</span>' +
-      '</a>',
-    disabledMoveButton:
-      '<span class="tui-page-btn tui-is-disabled tui-{{type}}">' +
-      '<span class="tui-ico-{{type}}">{{type}}</span>' +
-      '</span>',
-    moreButton:
-      '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
-      '<span class="tui-ico-ellip">...</span>' +
-      '</a>',
-  },
-};
-const pagination = new Pagination(container, pagOptions);
+// const container = document.getElementById('tui-pagination-container');
+
+// const pagOptions = {
+//   // below default value of options
+//   totalItems: 400,
+//   itemsPerPage: 20,
+//   visiblePages: 20,
+//   page: 1,
+//   centerAlign: false,
+//   firstItemClassName: 'tui-first-child',
+//   lastItemClassName: 'tui-last-child',
+//   template: {
+//     page: '<a href="#" class="tui-page-btn">{{page}}</a>',
+//     currentPage:
+//       '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
+//     moveButton:
+//       '<a href="#" class="tui-page-btn tui-{{type}}">' +
+//       '<span class="tui-ico-{{type}}">{{type}}</span>' +
+//       '</a>',
+//     disabledMoveButton:
+//       '<span class="tui-page-btn tui-is-disabled tui-{{type}}">' +
+//       '<span class="tui-ico-{{type}}">{{type}}</span>' +
+//       '</span>',
+//     moreButton:
+//       '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
+//       '<span class="tui-ico-ellip">...</span>' +
+//       '</a>',
+//   },
+// };
+// const pagination = new Pagination(container, pagOptions);
 
 // const instance = new Pagination(container, options);
 
 // const instance1 = new Pagination(container, { ... });
 
-console.log(pagination.getCurrentPage());
-pagination.setItemsPerPage(20);
-// pagination.paginationsetTotalItems(400);
-pagination.on('afterMove', event => {
-  const currentPage = event.page;
-  console.log(currentPage);
-});
+// console.log(pagination.getCurrentPage());
+// pagination.setItemsPerPage(20);
+// // pagination.paginationsetTotalItems(400);
+// pagination.on('afterMove', event => {
+//   const currentPage = event.page;
+//   console.log(currentPage);
+// });
 // pagination.on('beforeMove', event => {
 //   const currentPage = event.page;
 
